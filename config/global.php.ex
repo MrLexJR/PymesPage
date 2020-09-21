f8b4406f8127971d4c3272a617ef95e4994e4323
Jonathan <jonarsa_13@hotmail.com>
2020-09-21 11:30:45 -0500

    add example gobal

---
+++ config/global.php.ex
@@ -0,0 +1,21 @@
+<?php 
+//ip de la pc servidor base de datos
+define("DB_HOST", "localhost");
+
+// nombre de la base de datos
+define("DB_NAME", "namebd");
+
+
+//nombre de usuario de base de datos
+define("DB_USERNAME", "user");
+
+//conraseña del usuario de base de datos
+define("DB_PASSWORD", "password");
+
+//codificacion de caracteres
+define("DB_ENCODE", "utf8");
+
+//nombre del proyecto
+define("PRO_NOMBRE", "nameproject");
+ 
+ ?>
